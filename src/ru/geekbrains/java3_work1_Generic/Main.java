8f8f5c8d9d675184de7f8692ae74f81d17b92374
MigSer13 <avto470@mail.ru>
2021-03-17 00:39:50 +0300

    change1

--- src/ru/geekbrains/java3_work1_Generic/Main.java
+++ src/ru/geekbrains/java3_work1_Generic/Main.java
@@ -1,28 +1,43 @@
 package ru.geekbrains.java3_work1_Generic;
 
 import java.util.ArrayList;
+import java.util.List;
 
 public class Main {
 
-    public static void main(String[] args) {
-        ArrayList<Integer> arr = new ArrayList<Integer>();
-        arr.add(5);
-        arr.add(10);
-        arr.add(15);
-        arr.add(20);
+    public static void main(String[] args)
+    {
+        ArrayList<String> arr = new ArrayList<String>();
+        arr.add("a");
+        arr.add("b");
+        arr.add("c");
+        arr.add("d");
         System.out.println(arr);
 
-        swapArrayElements(1,2, arr);
+        new AnyArray<ArrayList>().swapArrayElements(1, 2, arr);
         System.out.println(arr);
     }
 
-    public static <T> void swapArrayElements(int e1, int e2, ArrayList<T> ArrObj) {
-        T element1 = ArrObj.get(e1);
-        T element2 = ArrObj.get(e2);
+//    public static <T> void swapArrayElements(int e1, int e2, <T>ArrObj)
+//    {
+//        T element1 = ArrObj.get(e1);
+//        T element2 = ArrObj.get(e2);
+//
+//        ArrObj.set(e1, element2);
+//        ArrObj.set(e2, element1);
+//
+//    }
 
-        ArrObj.set(e1, element2);
-        ArrObj.set(e2, element1);
+    public static class AnyArray<T extends List> {
 
+        public void swapArrayElements(int e1, int e2, T receivedArray)
+        {
+            Object element1 = receivedArray.get(e1);
+            Object element2 = receivedArray.get(e2);
+
+            receivedArray.set(e1, element2);
+            receivedArray.set(e2, element1);
+        }
     }
 }
 
